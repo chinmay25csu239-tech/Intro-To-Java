b5f843d3e053786c0da86360db3ec81c9781e0fd
chinmay25csu239-tech <chinmay25csu239@ncuindia.edu>
2026-07-28 15:40:37 +0530

    Create print(HelloWorld).java

---
+++ print(HelloWorld).java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        short semester = 4;                  
+        short admissionYear = 2026;         
+        int rollNumber = 12045;             
+        long mobileNumber = 9876543210L;
+        float attendancePercentage = 85.5f;
+        float cgpa = 9.25f;                 
+        char section = 'A';     
+        boolean staysInHostel = true;  
+
+        System.out.println("--- Student Record ---");
+        System.out.println("Semester: " + semester);
+        System.out.println("Admission Year: " + admissionYear);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Mobile Number: " + mobileNumber);
+        System.out.println("Attendance: " + attendancePercentage + "%");
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Section: " + section);
+        System.out.println("Stays in Hostel: " + staysInHostel);
+    }
+}
