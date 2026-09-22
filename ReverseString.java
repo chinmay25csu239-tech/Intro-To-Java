3699eb735bcd7884e06a1d6049cddbb4fa06d291
Chinmay Patil <chinmay25csu239@ncuindia.edu>
2026-09-22 14:40:35 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,8 @@
+public class ReverseString{
+  public static void main(String[] args){
+    String s ="Hello";
+    String rev ="";
+    for(int i=s.length()-1;i>=0;i--) rev +=s.charAt(i);
+    System.out.println("Reversed string:" + rev);
+  }
+  
